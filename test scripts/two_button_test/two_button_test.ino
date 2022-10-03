81b20fcc768f81e97b73f5717dddad71987581a8
Lexie Scholtz <lexiescholtz@gmail.com>
2022-10-03 10:11:39 -0700

    added test file for buttons

---
+++ test scripts/two_button_test/two_button_test.ino
@@ -0,0 +1,54 @@
+/*
+ * Test the Button class files
+ * Lexie Scholtz
+ * Created: 2022.10.02
+ * Last updated: 2022.10.03
+ */
+
+#include "Button.h"
+
+Button red = Button();
+Button green = Button(0x60);
+
+void setup() {
+  Serial.begin(9600);
+  // TODO: add wait statement to allow Serial time to begin
+  Serial.println("Testing Button class");
+
+  Wire.begin(); // MUST call this before using any i2c devices (including Button)
+
+  if (green.init() == false) {
+    Serial.println("Green button not connected! Reset when reconnected.");
+    while(1);
+  }
+
+  if (red.init() == false) {
+    Serial.println("Red button not connected! Reset when reconnected.");
+    while(1);
+  }
+
+  Serial.println("Buttons connected successfully!");
+}
+
+void loop() {
+  int red_status = red.update_status();
+  int green_status = green.update_status();
+  if (red_status > -1) { // button either clicked or pressed
+    if (red_status > LONG_HOLD_THRESHOLD) {
+      Serial.println("red long hold!");
+    } else if (red_status > SHORT_HOLD_THRESHOLD) {
+      Serial.println("red short hold");
+    } else if (red_status > CLICK_THRESHOLD) {
+      Serial.println("red click");
+    }
+  }
+  if (green_status > -1) { // button either clicked or pressed
+    if (green_status > LONG_HOLD_THRESHOLD) {
+      Serial.println("green long hold!");
+    } else if (green_status > SHORT_HOLD_THRESHOLD) {
+      Serial.println("green short hold");
+    } else if (green_status > CLICK_THRESHOLD) {
+      Serial.println("green click");
+    }
+  }
+}
