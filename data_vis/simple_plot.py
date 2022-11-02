904bb764701f67cbe47241a749e20b05a739a970
Lexie Scholtz <lexiescholtz@gmail.com>
2022-11-02 09:34:23 -0700

    Script to plot data from a series of files

---
+++ data_vis/simple_plot.py
@@ -0,0 +1,51 @@
+# Makes a simple plot of the data
+# Lexie Scholtz
+# Created: 2022.11.02
+
+# Usage:
+# $ python3 simple_plot.py <path> <files_names>
+#
+# Multiple file names may be given as long as they are at the same path.
+# For example: $ python3 simple_plot.py ../test_data/ file1.txt file2.txt
+#
+# For multiple paths, use ./ as the <path> and include the full path name in
+# each entry of <file_names>
+# For example: $ python3 simple_plot.py ./ dir1/file1.txt dir2/file2.txt
+
+import sys
+import numpy as np
+from matplotlib import pyplot as plt
+
+# check for correct number of arguments
+if len(sys.argv) < 3:
+    print("ERROR: Not enough arguments.")
+    print("Correct usage:")
+    print("$ python3 simple_plot.py <path> <file_names>")
+    sys.exit()
+
+# parse arguments
+path = sys.argv[1]
+file_list = sys.argv[2:] # get the file names as arguments
+
+# make sure path is formatted properly
+if path[-1] != '/':
+    path += '/'
+
+for file in file_list:
+    # error checking - make sure it is a .txt file
+    if not ".txt" in file:
+        file += ".txt"
+
+    file_path = path + file
+
+    # get points from file
+    data = np.genfromtxt(file_path)
+
+    # plot data
+    plt.plot(data[:, 0], data[:, 1], label=file)
+
+plt.title("QDM Data")
+plt.xlabel("Time (s)")
+plt.ylabel("Lux")
+plt.legend(loc='lower right')
+plt.show()
