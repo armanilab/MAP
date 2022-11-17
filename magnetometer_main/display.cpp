6f8b812d908f64754902a6405c8c43d1ead64519
StickyVic21 <vcnunez@usc.edu>
2022-11-17 13:25:16 -0800

    Commented out Serials: I believe?

--- magnetometer_main/display.cpp
+++ magnetometer_main/display.cpp
@@ -1,5 +1,6 @@
 /* class definition: Display */
-#include "Display.h"
+
+// #include "Display.h"
 
 /*
 Display::Display()
@@ -13,7 +14,7 @@ Display::Display()
 
 }
 */
-
+/*
 void Display::begin() {
   Serial.println("in Display::begin()");
   tft.init(135, 240); // Init ST7789 240x135
@@ -445,3 +446,4 @@ void Display::show_connection_re_established(char re_established[]) // displays 
     tft.println("Test resetting...");
 
 }
+*/
