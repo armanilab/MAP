05bb946f1964794ad7d3f560d82ea11e023122e4
Lexie Scholtz <lexiescholtz@gmail.com>
2023-07-03 10:27:45 -0600

    fixed file manager integration

--- code/data_vis/FileManagerClass.py
+++ code/data_vis/FileManagerClass.py
@@ -11,6 +11,13 @@ class FileManager:
             self.df = self.pre_process(self.df)
 
         self.plot_list = [] # initialize to_plot list as empty list
+        self.filtered_list = self.df.index.to_list()
+
+        # user indices
+        # TODO: eventually, set these in a menu when file is imported
+        self.file_loc_index = 1
+        self.tree_col_start = 1
+        self.tree_col_end = -2
 
     # read in the excel data log file, preprocess it,
     def load_data_log(self, file):
@@ -62,18 +69,49 @@ class FileManager:
         return df
 
     # arguments:
-    #   files - a list of files to be added to the plot_list
-    def add_to_plot_list(self, files):
-        for f in files:
-            if f not in self.plot_list:
-                self.plot_list.append(int(f))
+    #  [int] file_index - the index of a file to be added to the plot_list
+    def add_to_plot_list(self, file_index):
+        if file_index not in self.plot_list:
+            self.plot_list.append(file_index)
 
     # arguments:
-    #   files - a list of files to be removed from the plot_list
-    def remove_from_plot_list(self, files):
-        for f in files:
-            if f in self.plot_list:
-                self.plot_list.remove(int(f))
+    #   [int] file_index - the index of a file to be removed from the plot_list
+    def remove_from_plot_list(self, file_index):
+        if file_index in self.plot_list:
+            self.plot_list.remove(file_index)
 
+    # returns the to_plot_list (indices of files to be plotted)
     def get_plot_list(self):
         return self.plot_list
+
+    def get_tree_cols(self):
+        return self.df.columns[self.tree_col_start:self.tree_col_end]
+
+    def get_file_loc_col(self):
+        return self.df.columns[self.file_loc_index]
+
+    # apply a filter to the dataframe
+    # arguments:
+    #
+    def filter_df(self, filter_type=None, filter_selection=None):
+        # if no filter is passed, then it is the unfiltered df
+        if filter_type == None:
+            self.filtered_list = self.df.index.to_list()
+        else:
+            entries = self.df.loc[self.df[filter_type] == filter_selection]
+            self.filtered_list = entries.index.to_list()
+
+    def get_filter_options(self, filter_type):
+        filter_options = self.df[filter_type].fillna("")
+        return filter_options.unique().tolist()
+
+    # returns the filtered list
+    def get_filtered_list(self):
+        return self.filtered_list
+
+    def get_tree_row(self, row_index):
+        row = self.df.loc[row_index]
+        row_text = row['File-name']
+        # TODO: set nan to ""
+        row_value = tuple(row[self.tree_col_start:self.tree_col_end].fillna(""))
+        return row_text, row_value
--- code/data_vis/data_vis_gui.py
+++ code/data_vis/data_vis_gui.py
@@ -45,10 +45,10 @@ class MapDAP:
         # save the data file
 
         self.file = "../../../../test_data/paper_data/MAP_test_log_data.xlsx"
-        self.file_manager = FileManager(self.file)
-        self.df = self.file_manager.get_df()
-        self.filtered_list = self.df.index # start with ALL indices
-        self.to_plot_list = [] # start with none
+        self.fm = FileManager(self.file)
+        self.df = self.fm.get_df()
+        #self.filtered_list = self.df.index # start with ALL indices
+        #self.to_plot_list = [] # start with none
 
         # TODO: did this even do anything?? set button styles
         s = ttk.Style()
@@ -76,11 +76,11 @@ class MapDAP:
         self.file_preview_frame.rowconfigure(0, weight=1)
 
         # view files from treeview
-        cols = self.df.columns[1:-2]
+        cols = self.fm.get_tree_cols()
         self.full_file_tree = self.create_tree(self.file_preview_frame)
 
         # add rows to tree from the dataframe (the file log)
-        self.update_tree(self.full_file_tree, self.filtered_list,
+        self.update_tree(self.full_file_tree, self.fm.get_filtered_list(),
             highlight=True)
 
         # set appearance for selected items
@@ -159,7 +159,7 @@ class MapDAP:
             self.selection_preview_frame)
 
         # add rows to tree from the dataframe (the file log)
-        self.update_tree(self.selection_preview_tree, self.to_plot_list)
+        self.update_tree(self.selection_preview_tree, self.fm.get_plot_list())
 
         # TODO: connect command to navigate to plot screen
         self.plot_button = ttk.Button(self.selection_page, text="Plot",
@@ -167,15 +167,16 @@ class MapDAP:
         self.plot_button.grid(row=6, column=3, rowspan=2, columnspan=2)
 
     def create_tree(self, frame, ht=10):
-        cols = self.df.columns[1:-2]
+        cols = self.fm.get_tree_cols()
         tree = ttk.Treeview(frame, columns=tuple(cols), height=ht)
         tree.column('#0', width=120)
         tree.heading('#0', text='File-name')
 
         # set column widths
         # first "column" is file-location - should be longer
-        tree.column(self.df.columns[1], width=200, anchor='w')
-        tree.heading(self.df.columns[1], text=self.df.columns[1])
+        tree.column(self.fm.get_file_loc_col(), width=200, anchor='w')
+        tree.heading(self.fm.get_file_loc_col(),
+            text=self.fm.get_file_loc_col())
 
         # set all other columns to width of 100
         for i in (range(len(cols[1:]))):
@@ -197,12 +198,14 @@ class MapDAP:
 
         # add new entries from given dataframe
         for i in indices:
-            row = self.df.loc[i]
-            tree.insert('', 'end', id=i, text=row['File-name'],
-                value=tuple(row[1:-2]))
+            row_text, row_value = self.fm.get_tree_row(i)
+            tree.insert('', 'end', id=i, text=row_text, value=row_value)
+            #row = self.df.loc[i]
+            #tree.insert('', 'end', id=i, text=row['File-name'],
+            #    value=tuple(row[1:-2]))
 
             if highlight:
-                if i in self.to_plot_list:
+                if i in self.fm.get_plot_list():
                     tree.item(i, tags=('selected'))
 
     def add_selected(self):
@@ -215,12 +218,13 @@ class MapDAP:
             item_index = int(item)# convert item id to the index
 
             # add index from the list of files to be plotted
-            if item_index not in self.to_plot_list:
-                self.to_plot_list.append(item_index)
+            self.fm.add_to_plot_list(item_index)
+            #if item_index not in self.to_plot_list:
+            #    self.to_plot_list.append(item_index)
 
         # update the selection preview tree
-        self.update_tree(self.selection_preview_tree, self.to_plot_list)
-        self.update_tree(self.file_list_tree, self.to_plot_list)
+        self.update_tree(self.selection_preview_tree, self.fm.get_plot_list())
+        self.update_tree(self.file_list_tree, self.fm.get_plot_list())
 
     def remove_selected(self):
         selected_items = self.full_file_tree.selection()
@@ -232,43 +236,41 @@ class MapDAP:
             item_index = int(item) # convert item id to the index
 
             # remove index from the list of files to be plotted
-            if item_index in self.to_plot_list:
-                self.to_plot_list.remove(item_index)
+            self.fm.remove_from_plot_list(item_index)
+            #if item_index in self.to_plot_list:
+            #    self.to_plot_list.remove(item_index)
 
         # update the selection preview tree
-        self.update_tree(self.selection_preview_tree, self.to_plot_list)
-        self.update_tree(self.file_list_tree, self.to_plot_list)
+        self.update_tree(self.selection_preview_tree, self.fm.get_plot_list())
+        self.update_tree(self.file_list_tree, self.fm.get_plot_list())
 
     def add_all(self):
         # add all shown files to the to_plot_list
-        for i in self.filtered_list:
-            if i not in self.to_plot_list:
-                self.to_plot_list.append(i)
+        for i in self.fm.get_filtered_list():
+            self.fm.add_to_plot_list(i)
             # add highlight to file in full_file_tree
             self.full_file_tree.item(i, tags=('selected'))
 
         # update the selection preview tree
-        self.update_tree(self.selection_preview_tree, self.to_plot_list)
-        self.update_tree(self.file_list_tree, self.to_plot_list)
+        self.update_tree(self.selection_preview_tree, self.fm.get_plot_list())
+        self.update_tree(self.file_list_tree, self.fm.get_plot_list())
 
     def remove_all(self):
         # remove all shown files from the to_plot_list
-        for i in self.filtered_list:
-            if i in self.to_plot_list:
-                self.to_plot_list.remove(i)
+        for i in self.fm.get_filtered_list():
+            self.fm.remove_from_plot_list(i)
             # remove highlight of file in full_file_tree
             self.full_file_tree.item(i, tags=())
 
         # update the selection preview tree
-        self.update_tree(self.selection_preview_tree, self.to_plot_list)
-        self.update_tree(self.file_list_tree, self.to_plot_list)
+        self.update_tree(self.selection_preview_tree, self.fm.get_plot_list())
+        self.update_tree(self.file_list_tree, self.fm.get_plot_list())
 
     #TODO: fix the sort so that if they're numerical, then the sort by numbers
     def display_filter_options(self, event):
         # get options from dataframe for this filter type
         filter_type = self.filter_var.get()
-        filter_options = self.df[filter_type].fillna("")
-        self.filter_options = filter_options.unique().tolist()
+        self.filter_options = self.fm.get_filter_options(filter_type)
         self.filter_options.sort()
         self.filter_options.append("<Blank>") # for empty cells. #TODO: rename this?
 
@@ -298,15 +300,16 @@ class MapDAP:
         print("value: " + filter_selected)
 
         # select the subset of the dataframe
-        entries = self.df.loc[self.df[filter_type] == filter_selected]
-        self.filtered_list = entries.index.to_list()
-        print(entries)
-        self.update_tree(self.full_file_tree, self.filtered_list,
+        self.fm.filter_df(filter_type, filter_selected)
+        #entries = self.df.loc[self.df[filter_type] == filter_selected]
+        #self.filtered_list = entries.index.to_list()
+        #print(entries)
+        self.update_tree(self.full_file_tree, self.fm.get_filtered_list(),
             highlight=True)
 
     def reset_filter(self):
-        self.filtered_list = self.df.index
-        self.update_tree(self.full_file_tree, self.filtered_list,
+        self.fm.filter_df()
+        self.update_tree(self.full_file_tree, self.fm.get_filtered_list(),
             highlight=True)
 
     def create_plot_page(self):
@@ -336,7 +339,7 @@ class MapDAP:
 
         # create treeview to see selected files
         self.file_list_tree = self.create_tree(self.file_list_frame, ht=5)
-        self.update_tree(self.file_list_tree, self.to_plot_list)
+        self.update_tree(self.file_list_tree, self.fm.get_plot_list())
 
         # TODO: add scrollbar to treeview
 
