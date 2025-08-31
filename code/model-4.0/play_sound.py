f03f87b70c397b946666be0ec61c7023c00ee3a6
Lexie Scholtz <lexiescholtz@gmail.com>
2025-08-30 22:38:55 -0700

    first verison of model 4

---
+++ code/model-4.0/play_sound.py
@@ -0,0 +1,7 @@
+
+from playsound import playsound
+
+i = 0
+while i < 3:
+    playsound('notify.aiff')
+    i += 1
---
+++ code/model-4.0/run_revision_fits.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# 8/26
+printf "MAP_log_revisions\n826_01\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_02\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_03\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_04\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_05\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_06\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_07\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_08\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_09\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_10\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_11\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_12\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_13\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_14\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_15\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_16\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_17\n" | python3 model_fits.py
+printf "MAP_log_revisions\n826_18\n" | python3 model_fits.py
+
+# 8/27
+printf "MAP_log_revisions\n827_01\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_02\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_03\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_04\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_05\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_06\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_07\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_08\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_09\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_10\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_11\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_12\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_13\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_14\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_15\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_16\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_17\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_18\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_19\n" | python3 model_fits.py
+printf "MAP_log_revisions\n827_20\n" | python3 model_fits.py
+
+
+
+python3 play_sound.py
+echo ""
+echo "finished analysis."
